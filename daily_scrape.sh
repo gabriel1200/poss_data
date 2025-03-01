729d9955ace1e5f843f36b572e944e6c7d451215
gabriel1200 <gabe.r.guzman@gmail.com>
2025-03-01 15:11:25 -0600

    quick commit

--- daily_scrape.sh
+++ daily_scrape.sh
@@ -2,12 +2,12 @@
 
 # Convert Jupyter notebooks to Python scripts in their respective directories
 
-jupyter nbconvert --to script rotation_scrape.ipynb
+jupyter nbconvert --to script rotations_scrape.ipynb
 jupyter nbconvert --to script clip_scrape.ipynb
 
 jupyter nbconvert --to script clip_merge2.ipynb
 jupyter nbconvert --to script generate_opp.ipynb
-python rotation_scrape.py
+python rotations_scrape.py
 python clip_scrape.py
 python clip_merge2.py
 python generate_opp.py
